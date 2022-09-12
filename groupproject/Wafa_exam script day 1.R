324805e5a8278fe9e92ab1885251024e28b10a70
Wafa Aftab <wafa.aftab@uib.no>
2022-09-12 12:26:32 +0200

    Checked whether any columns have more than one variable

--- groupproject/Wafa_exam script day 1.R
+++ groupproject/Wafa_exam script day 1.R
@@ -4,6 +4,9 @@ read_delim (here("groupproject", "exam_nontidy.txt"))
 glimpse(data) # checking whether any column starts with a number or space 
 
 
+# Are there any colmuns containing combined variables 
+view(data)
+# the second last column i.e. volume measurement has two types pvol and tvol which shold be separate columns/variables 
 
 
 
