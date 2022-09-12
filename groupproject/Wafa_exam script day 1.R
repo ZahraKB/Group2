fd3bc4a5ff993be51f7f14d206ce871a814c302e
Wafaftab <97549731+Wafaftab@users.noreply.github.com>
2022-09-12 12:23:34 +0200

    Update Wafa_exam script day 1.R

--- groupproject/Wafa_exam script day 1.R
+++ groupproject/Wafa_exam script day 1.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(here)
 read_delim (here("groupproject", "exam_nontidy.txt"))
 glimpse(data) # checking whether any column starts with a number or space 
+# yes, the volume measurement variable has a space in the variable name 
 
 
 
