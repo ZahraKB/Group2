a66d4142abd7bf1e6478724c78a0e28606be59d4
karolynna009 <caroliinaa009@gmail.com>
2022-09-12 12:30:47 +0200

    groupproject

--- groupproject/Carol.R
+++ groupproject/Carol.R
@@ -4,4 +4,4 @@ glimpse (data)
 head(data)
 #no not all varibles are columns. The varibale .value needs to be split into new columns pvol and and tvol which will be created from the volume measurement column.m 
 view (data)
-testing123
+testing1234
