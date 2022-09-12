d1e3842dabe7a25707b2dd59a31ba39ff751d7b2
Wafa Aftab <wafa.aftab@uib.no>
2022-09-12 12:04:25 +0200

    Checking whether any $ start with # or space

---
+++ groupproject/Wafa_exam script day 1.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(here)
+read_delim (here("groupproject", "exam_nontidy.txt"))
+glimpse(data) # checking whether any column starts with a number or space 
+
+
+
+
+
+
+           
